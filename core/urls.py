c802c44e956a76742b0d3fafe44fd99397cdbea1
pinkishsabito <ozhetov.arsen@gmail.com>
2023-12-01 18:11:05 +0600

    feat: error (somewhere, please Adik fix it)

--- core/urls.py
+++ core/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.template.context_processors import static
-from django.urls import path
+from django.urls import path, include
 
 from core import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('qonaq.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL)
